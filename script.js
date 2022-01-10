c68a3908ca33196b0991bac035ff6df1328fe4d3
jnnh <jenniferhorvath@cmail.carleton.ca>
2022-01-10 04:21:38 -0500

    problems with loading local storage

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var tasks = {};
+var taskIDs =["block9","block10","block11","block12","block13","block14","block15","block16","block17"];
 
 $(document).ready(function(){
     var date = moment().format("dddd MMM Do YYYY");
@@ -8,18 +8,18 @@ $(document).ready(function(){
 $(".saveBtn").on("click", function(){
     var taskEl = $(this).closest(".time-block").find(".description");
     var textInput =taskEl.val();
+    var taskId = taskEl.attr("id");
     $(taskEl).attr("value", textInput);
-    console.log(taskEl);
+   // Save to local storage
+    localStorage.setItem(taskId,textInput);
 });
-//save to local Storage
-var saveTasks = function(){
-    localStorage.setItem("tasks",JSON.stringify(tasks));
-};
 //load tasks from local Storage
 var loadTasks = function(){
-    tasks = JSON.parse(localStorage.getItem("tasks"));
-    $.each(tasks, )
-}
+    taskIDs.forEach(function(element){
+        $("'#"+element+"'").val(localStorage.getItem(element));
+    });
+};
+
 
 var auditTime = function (timeBlockEl){
     var hour = $(timeBlockEl).find(".hour").attr("id").replace("hr", "");
@@ -34,6 +34,9 @@ var auditTime = function (timeBlockEl){
         $(timeBlockEl).addClass("future");
     }
 }
+
+loadTasks();
+
 
 // setTimeout(function(){
 //     $(".time-block").each(function(index, el){
