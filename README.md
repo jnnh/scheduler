e3936a9de712b0778264050d5c5fa6a16f506ddf
jnnh <jenniferhorvath@cmail.carleton.ca>
2022-01-10 04:50:05 -0500

    working load local storage

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # scheduler
+
+## Purpose
+To create a daily planner with events that can be edited and saved into local storage. Each time-slot is colour-coded to indicate whether it is in the past, present, or future.
+
+## Screenshot
+
+
+## Built With
+*HTML 
+*CSS
+*JSS
+
+## Additional Resources
+*jQuery
+*Bootstrap
+*Moment.js
+*Iconic
+*Google fonts
+*Font awesome
+
+## Website
+
+
+## Contribution
+Made by Jennifer Horvath
--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 var taskIDs =["block9","block10","block11","block12","block13","block14","block15","block16","block17"];
 
-$(document).ready(function(){
-    var date = moment().format("dddd MMM Do YYYY");
-    $("#currentDay").text(date);
-});
 //save Button clicked
 $(".saveBtn").on("click", function(){
     var taskEl = $(this).closest(".time-block").find(".description");
@@ -16,30 +12,37 @@ $(".saveBtn").on("click", function(){
 //load tasks from local Storage
 var loadTasks = function(){
     taskIDs.forEach(function(element){
-        $("'#"+element+"'").val(localStorage.getItem(element));
+        $(`#${element}`).val(localStorage.getItem(element));
     });
 };
-
 
 var auditTime = function (timeBlockEl){
     var hour = $(timeBlockEl).find(".hour").attr("id").replace("hr", "");
     var time = moment().hour();
     if (hour<time){
+        $(timeBlockEl).removeClass("present future");
         $(timeBlockEl).addClass("past");
     }
     else if (hour===time){
+        $(timeBlockEl).removeClass("past future");
         $(timeBlockEl).addClass("present");
     }
     else{
+        $(timeBlockEl).removeClass("present past");
         $(timeBlockEl).addClass("future");
     }
 }
 
+$(document).ready(function(){
+    var date = moment().format("dddd MMM Do YYYY");
+    $("#currentDay").text(date);
+});
+
 loadTasks();
 
 
-// setTimeout(function(){
+// setInterval(function(){
 //     $(".time-block").each(function(index, el){
 //       auditTime(el);
 //     });
-//   }, 1000);
+//   }, (1000*60)*30;
